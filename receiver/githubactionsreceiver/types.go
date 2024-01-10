1b16980f9a9a20860189c5359508a63909aa5685
Bart X Tubalinal <bxtp4p@users.noreply.github.com>
2024-01-10 20:59:18 +0000

    updates

--- receiver/githubactionsreceiver/types.go
+++ receiver/githubactionsreceiver/types.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package githubactionsreceiver
+package githubactionsreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/githubactionsreceiver"
 
 import "time"
 
